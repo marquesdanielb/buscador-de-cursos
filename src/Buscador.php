d6616d47fc93125225543d8048d1cf2ca5a1347c
Daniel Marques Barbosa <marquesdanielb@gmail.com>
2024-11-22 08:19:03 -0300

    adiciona scripts automatizados

--- src/Buscador.php
+++ src/Buscador.php
@@ -9,18 +9,19 @@ class Buscador
 {
     public function __construct(
         private ClientInterface $httpClient,
-        private Crawler $crawler,
-    ) {}
+        private Crawler $crawler
+    ) {
+    }
 
     public function buscar(string $url): array
     {
         $resposta = $this->httpClient->request('GET', $url);
         $html = $resposta->getBody();
-        $this->crawler->addHtmlContent($html);
-        
-        $elementos = $this->crawler->filter('span.card-curso__nome');
-        $cursos = [];
 
+        $this->crawler->addHtmlContent($html);
+        $elementos = $this->crawler->filter('span.card-curso__nome');
+
+        $cursos = [];
         foreach ($elementos as $elemento) {
             $cursos[] = $elemento->textContent;
         }
